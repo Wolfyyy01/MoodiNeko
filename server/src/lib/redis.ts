ca700ad5a24c61a7f75d333b1e22ebf8d8babe10
Wolfy01 <64611391+Wolfyyy01@users.noreply.github.com>
2025-05-17 16:27:33 +0300

    Update redis.ts

--- server/src/lib/redis.ts
+++ server/src/lib/redis.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 
-export const redis = new Redis({
+export const redis = new Redis(process.env.REDIS_URL || {
   host: `${process.env.REDIS_HOST || "redis"}`,
   port: Number(process.env.REDIS_PORT) || 6379,
   password: process.env.REDIS_PASSWORD,
