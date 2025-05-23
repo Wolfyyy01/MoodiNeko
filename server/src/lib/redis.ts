baf7f4de64933cc8962133c5e3d60ad232c1d381
Wolfy01 <64611391+Wolfyyy01@users.noreply.github.com>
2025-05-24 00:55:35 +0300

    Fix connection method using redis url

--- server/src/lib/redis.ts
+++ server/src/lib/redis.ts
@@ -1,10 +1,12 @@
 import Redis from "ioredis";
 
-export const redis = new Redis(process.env.REDIS_URL || {
-  host: `${process.env.REDIS_HOST || "redis"}`,
-  port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASSWORD,
-});
+export const redis = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL)
+  : new Redis({
+      host: process.env.REDIS_HOST || "localhost",
+      port: Number(process.env.REDIS_PORT) || 6379,
+      password: process.env.REDIS_PASSWORD,
+    });
 
 // Connect to Redis
 redis.on("connect", () => {
