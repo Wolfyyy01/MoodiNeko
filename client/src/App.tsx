a08199fd0f23fd90ce3d9fb39bf751279eab840c
Wolfy01 <64611391+Wolfyyy01@users.noreply.github.com>
2025-10-18 14:48:06 +0300

    Add Analytics component to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from 'react';
 import { motion } from 'framer-motion';
+import { Analytics } from "@vercel/analytics/next"
 import FloatingCrystal from './components/FloatingCrystal';
 
 import logo from './assets/logo.png';
@@ -120,6 +121,7 @@ export default function App() {
       )}
 
       <Footer />
+      <Analytics />
     </div>
 
   );
