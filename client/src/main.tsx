876b1709cf742c9d625c2186315f89f5e9677cea
Wolfy01 <64611391+Wolfyyy01@users.noreply.github.com>
2025-10-18 14:51:54 +0300

    Remove Analytics component from main.tsx
    Removed Analytics component from the main render.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,14 +4,12 @@ import './index.css'
 import './styles.css'
 import App from './App.tsx'
 import logo from './assets/logo.png'
-import { Analytics } from '@vercel/analytics/react'
 import { ToastProvider } from './context/ToastContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ToastProvider>
       <App />
-      <Analytics />
     </ToastProvider>
   </StrictMode>,
 )
